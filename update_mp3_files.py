6853a2060b405c5ebf16fc1e88c06b414d6b079d
HiroakiOkada <kryou649@gmail.com>
2023-09-17 04:41:59 +0900

    Add TPE1

--- update_mp3_files.py
+++ update_mp3_files.py
@@ -1,18 +1,19 @@
 import pdb
 
 import os
-from mutagen.id3 import ID3, TIT2, TRCK, TALB, TYER
+from mutagen.id3 import ID3, TIT2, TRCK, TALB, TYER, TPE1
 
 
 #----- PARAMETERS -----#
 folder_path = os.getcwd()
 album_name = 'xxx'
+artist_name = 'yyy'
 year = 2023
 order = 'update_time'
 #----------------------#
 
 
-# 指定されたフォルダ内のMP3ファイルを取得
+# 指定されたフォルダ内の MP3 ファイルを取得
 def get_files(folder_path, order):
     # 名前順で取得
     if order == 'name':
@@ -31,7 +32,7 @@ def get_track_number_list(files):
     return [i for i in range(1, len(files) + 1)]
 
 
-# MP3ファイルのアップデート
+# MP3 ファイルのアップデート
 def update_mp3_file(folder_path, album_name, year, order):
     files = get_files(folder_path, order)
     track_number_l = get_track_number_list(files)
@@ -42,9 +43,10 @@ def update_mp3_file(folder_path, album_name, year, order):
         # ファイルのプロパティを変更
         audio = ID3(file_path)
         title = file_name.split('.')[0]
-        audio['TIT2'] = TIT2(encoding=3, text=title)              # タイトルを設定する
-        audio['TRCK'] = TRCK(encoding=3, text=str(track_number))  # トラック番号を設定する
-        audio['TALB'] = TALB(encoding=3, text=album_name)         # アルバム名を設定する
+        audio['TIT2'] = TIT2(encoding=3, text=title)              # タイトルを設定
+        audio['TRCK'] = TRCK(encoding=3, text=str(track_number))  # トラック番号を設定
+        audio['TALB'] = TALB(encoding=3, text=album_name)         # アルバム名を設定
+        audio['TPE1'] = TPE1(encoding=3, text=artist_name)        # アーティスト名を設定
         audio['TYER'] = TYER(encoding=3, text=str(year))          # 年を設定
         audio.save()                                              # 保存
 
